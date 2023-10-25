064af3d94ba75b48e011a614ca08e901a575520f
canon0826 <147060718+canon0826@users.noreply.github.com>
2023-10-26 06:19:36 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  root to: "messages#index"
+  
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
